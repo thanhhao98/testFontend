e23e5bd465101ab4669d9668b1522f0bb52aa1db
ThanhHao <hao.phanthanh98@gmail.com>
2019-04-18 13:39:26 +0700

    remove unneed route

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import LatestMovie from '@/components/LatestMovie'
-import Movie from '@/components/Movie'
-import SearchMovie from '@/components/SearchMovie'
+// import LatestMovie from '@/components/LatestMovie'
+// import Movie from '@/components/Movie'
+// import SearchMovie from '@/components/SearchMovie'
 import home from '@/components/home'
 import Login from '@/components/Login'
+import Register from '@/components/Register'
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
     {
       path: '/',
-      name: 'LatestMovie',
-      component: LatestMovie
+      name: 'home',
+      component: home
     },
     {
       path: '/login',
@@ -20,22 +21,27 @@ export default new VueRouter({
       component: Login
     },
     {
-      path: '/home',
-      name: 'home',
-      component: home
-    },
-    {
-      path: '/movie/:id',
-      name: 'Movie',
-      props: true,
-      component: Movie
-    },
-    {
-      path: '/search/:name',
-      name: 'SearchMovie',
-      props: true,
-      component: SearchMovie
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
+    // {
+    //   path: '/home',
+    //   name: 'home',
+    //   component: home
+    // },
+    // {
+    //   path: '/movie/:id',
+    //   name: 'Movie',
+    //   props: true,
+    //   component: Movie
+    // },
+    // {
+    //   path: '/search/:name',
+    //   name: 'SearchMovie',
+    //   props: true,
+    //   component: SearchMovie
+    // }
   ],
   mode: 'history'
 })
